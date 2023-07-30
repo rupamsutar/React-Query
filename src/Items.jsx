56c7ca4412006b2e84d5f1b849fec9bc2c71c01f
Rupam Sutar <rupamsutar281998@gmail.com>
2023-07-30 20:16:18 +0530

    extra data removed from mapping

--- src/Items.jsx
+++ src/Items.jsx
@@ -5,16 +5,22 @@ import customFetch from './utils';
 
 const Items = ({ items }) => {
   
-  const result = useQuery({
+  const {isLoading, data} = useQuery({
     queryKey: [],
-    queryFn: () => customFetch.get('/')
+    queryFn: async() => {
+      const {data} = await customFetch.get('/');
+      return data;
+    }
   });
 
-    console.log(result);
+  if (isLoading) {
+    return <p style={{marginTop: '1rem'}}>Loading...</p>
+  }
+
   
   return (
     <div className='items'>
-      {items.map((item) => {
+      {data.taskList.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>
