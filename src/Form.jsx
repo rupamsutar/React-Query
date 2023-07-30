6b31f1cf144ed90197cdb8f3d3b45b38a93dfaab
Rupam Sutar <rupamsutar281998@gmail.com>
2023-07-30 23:36:54 +0530

    Toast Added.

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,6 +1,7 @@
 import { useMutation } from '@tanstack/react-query';
 import { useState } from 'react';
 import customFetch from './utils';
+import { toast } from 'react-toastify';
 
 const Form = () => {
   const [newItemName, setNewItemName] = useState('');
@@ -8,7 +9,14 @@ const Form = () => {
   const {mutate: createTask, isLoading: cTIsloading} = useMutation({
     mutationFn: (data) => customFetch.post('/', {
       title: data
-    })
+    }),
+    onSuccess: () => {
+
+    },
+
+    onError: (error) => {
+      toast.error(error.response.data.msg);
+    }
   });
 
 
