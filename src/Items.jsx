fab59a9b30ff29eecc579b521de322f32666c812
Rupam Sutar <rupamsutar281998@gmail.com>
2023-07-31 08:14:43 +0530

    update 2

--- src/Items.jsx
+++ src/Items.jsx
@@ -1,29 +1,22 @@
 import SingleItem from './SingleItem';
-import { useQuery } from '@tanstack/react-query';
-import customFetch from './utils';
+import { useFetchTask } from './ReactQueryCustomHooks';
 
 
 const Items = ({ items }) => {
-  
-  const {isLoading, data, isError, error} = useQuery({
-    queryKey: [],
-    queryFn: async() => {
-      const {data} = await customFetch.get('/');
-      return data;
-    }
-  });
+
+  const {isLoading, data, isError} = useFetchTask();
 
   if (isLoading) {
     return <p style={{marginTop: '1rem'}}>Loading...</p>
   }
 
-  // if (isError) {
-  //   return <p style={{marginTop: '1rem'}}>There was an error</p>
-  // }
-
-  if(error) {
-    return <p style={{marginTop: 'rem'}}>{error.message}</p>
+  if (isError) {
+    return <p style={{marginTop: '1rem'}}>There was an error</p>
   }
+
+  // if(error) {
+  //   return <p style={{marginTop: 'rem'}}>{error.message}</p>
+  // }
 
   
   return (
